630dcbdda057ee3ef3b62f9b0154fec17b7c8f74
dealerofallthecats <xhackerqwertyx@protonmail.com>
2025-08-15 21:36:34 +1200

    Fri 15 Aug 2025 21:36:34 NZST

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
   };
 
   outputs = { nixpkgs, ... }@inputs: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.hpnix = nixpkgs.lib.nixosSystem {
       modules = [ ./modules ];
       specialArgs = { inherit inputs; };
     };
