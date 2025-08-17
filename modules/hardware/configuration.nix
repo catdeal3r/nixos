2088b990f9b96301c81c93f45ba043bd0b68770a
dealerofallthecats <xhackerqwertyx@protonmail.com>
2025-08-18 09:32:21 +1200

    Mon 18 Aug 2025 09:32:21 AM

--- modules/hardware/configuration.nix
+++ modules/hardware/configuration.nix
@@ -50,11 +50,9 @@
     variant = "";
   };
 
-  # Add GNOME
+  # Add SDDM
   services.displayManager.sddm.wayland.enable = true;
-  # services.desktopManager.gnome.enable = true;
   
-
   # Delete old nixos versions
   nix.gc = {
     automatic = true;
@@ -66,24 +64,8 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  # Firewall.
+  networking.firewall.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
