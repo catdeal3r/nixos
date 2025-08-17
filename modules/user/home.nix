cd20d4ad591fbf3e3f6170633c766cf52e96969f
dealerofallthecats <xhackerqwertyx@protonmail.com>
2025-08-17 13:24:04 +1200

    Sun 17 Aug 2025 01:24:04 PM

--- modules/user/home.nix
+++ modules/user/home.nix
@@ -34,6 +34,14 @@
       };
 
       desktopEntries.firefox = {
+        name = "Firefox";
+        genericName = "Web Browser";
+        categories = [ "Network" "WebBrowser" ];
+        Icon = "Firefox";
+        mimeType = [ "text/html" "text/xml" "application/xhtml+xml" "application/vnd.mozilla.xul+xml" "x-scheme-handler/http" "x-scheme-handler/https" ];
+        startupNotify = true;
+        terminal = false;
+        type = "Application";
         actions = {
           "New Window" = {
             exec = "firefox --new-window %U -P";
