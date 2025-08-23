e6feb8231199d356f79419b43a4c492c0d62882b
dealerofallthecats <xhackerqwertyx@protonmail.com>
2025-08-23 15:18:47 +1200

    Sat 23 Aug 2025 03:18:47 PM

--- modules/apps/orchis.nix
+++ modules/apps/orchis.nix
@@ -7,7 +7,7 @@
   gtk-engine-murrine,
   sassc,
   border-radius ? 10, # Suggested: 2 < value < 16
-  tweaks ? [ ], # can be "solid" "compact" "black" "primary" "macos" "submenu" "nord|dracula"
+  tweaks ? [ "black" ], # can be "solid" "compact" "black" "primary" "macos" "submenu" "nord|dracula"
   withWallpapers ? false,
   ...
 }:
