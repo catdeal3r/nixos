9e5ebf431a0fd2962d4558a5af4be4687b6a5f5b
dealerofallthecats <xhackerqwertyx@protonmail.com>
2025-08-24 13:10:49 +1200

    Sun 24 Aug 2025 01:10:49 PM

--- modules/apps/flatpak.nix
+++ modules/apps/flatpak.nix
@@ -22,7 +22,7 @@
     ];
     
     overrides = {
-      global.Environment.XCURSOR_PATH = "/etc/profiles/per-user/catdealer/share/icons:/run/host/user-share/icons:/run/host/share/icons";
+      global.Environment.XCURSOR_PATH = "/home/catdealer/.icons:/home/catdealer/.local/share/icons:/home/catdealer/.local/share/flatpak/exports/share/icons:/home/catdealer/.local/share/flatpak/exports/share/pixmaps:/var/lib/flatpak/exports/share/icons:/var/lib/flatpak/exports/share/pixmaps:/home/catdealer/.nix-profile/share/icons:/home/catdealer/.nix-profile/share/pixmaps:/nix/profile/share/icons:/nix/profile/share/pixmaps:/home/catdealer/.local/state/nix/profile/share/icons:/home/catdealer/.local/state/nix/profile/share/pixmaps:/etc/profiles/per-user/catdealer/share/icons:/etc/profiles/per-user/catdealer/share/pixmaps:/nix/var/nix/profiles/default/share/icons:/nix/var/nix/profiles/default/share/pixmaps:/run/current-system/sw/share/icons:/run/current-system/sw/share/pixmaps";
     };
   };
 }
