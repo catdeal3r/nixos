4596959326da4e8984d28519e2b7d5f2f0859787
dealerofallthecats <xhackerqwertyx@protonmail.com>
2025-08-24 12:51:57 +1200

    Sun 24 Aug 2025 12:51:57 PM

--- modules/apps/flatpak.nix
+++ modules/apps/flatpak.nix
@@ -2,10 +2,12 @@
 
 {
   imports = [
-    inputs.flatpak-nix.nixosModule.nix-flatpak
+    inputs.flatpak-nix.nixosModules.nix-flatpak
   ];
 
   services.flatpak = {
+    enable = true;
+
     remotes = lib.mkOptionDefault [{
       name = "flathub";
       location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
