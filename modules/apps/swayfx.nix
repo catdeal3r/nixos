dc85633b8ded452e92c64b152c3ee7f4de5466ef
dealerofallthecats <xhackerqwertyx@protonmail.com>
2025-08-26 10:58:50 +1200

    Tue 26 Aug 2025 10:58:50 AM

--- modules/apps/swayfx.nix
+++ modules/apps/swayfx.nix
@@ -5,39 +5,39 @@
   programs.sway = {
     enable = true;
     package = pkgs.swayfx;
-
-    extraPackages = with pkgs; [
-      kitty
-      swaybg
-      swayidle
-      slurp
-      grim
-      wofi
-      matugen
-      nautilus
-      libnotify
-      wl-clipboard
-      glib
-      lxde.lxsession
-      gtk3
-      gtk4
-      brightnessctl
-      pulseaudio
-      # fonts
-      material-icons
-      material-symbols
-      nerd-fonts.hack
-      # quickshell
-      kdePackages.qt5compat
-      kdePackages.qtmultimedia
-      # try to fix gtk monke ass ahh file pick 
-      xdg-user-dirs-gtk
-      xdg-desktop-portal-gtk
-      gsettings-desktop-schemas
-      dconf-editor
-      dconf
-    ];  
   };
+
+  environment.systemPackages = with pkgs; [
+    kitty
+    swaybg
+    swayidle
+    slurp
+    grim
+    wofi
+    matugen
+    nautilus
+    libnotify
+    wl-clipboard
+    glib
+    lxde.lxsession
+    gtk3
+    gtk4
+    brightnessctl
+    pulseaudio
+    # fonts
+    material-icons
+    material-symbols
+    nerd-fonts.hack
+    # quickshell
+    kdePackages.qt5compat
+    kdePackages.qtmultimedia
+    # try to fix gtk monke ass ahh file picker
+    xdg-user-dirs-gtk
+    xdg-desktop-portal-gtk
+    gsettings-desktop-schemas
+    dconf-editor
+    dconf
+  ];
 
 
   programs.dconf.enable = true;
