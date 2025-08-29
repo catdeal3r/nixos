44d55d39c310fa272737897b3ef1a4075bc06270
dealerofallthecats <xhackerqwertyx@protonmail.com>
2025-08-30 11:14:56 +1200

    Sat 30 Aug 2025 11:14:56 AM

--- modules/apps/flatpak.nix
+++ modules/apps/flatpak.nix
@@ -25,9 +25,10 @@
       global.Environment.XCURSOR_PATH = "/home/catdealer/.icons:/home/catdealer/.local/share/icons:/home/catdealer/.local/share/flatpak/exports/share/icons:/home/catdealer/.local/share/flatpak/exports/share/pixmaps:/var/lib/flatpak/exports/share/icons:/var/lib/flatpak/exports/share/pixmaps:/home/catdealer/.nix-profile/share/icons:/home/catdealer/.nix-profile/share/pixmaps:/nix/profile/share/icons:/nix/profile/share/pixmaps:/home/catdealer/.local/state/nix/profile/share/icons:/home/catdealer/.local/state/nix/profile/share/pixmaps:/etc/profiles/per-user/catdealer/share/icons:/etc/profiles/per-user/catdealer/share/pixmaps:/nix/var/nix/profiles/default/share/icons:/nix/var/nix/profiles/default/share/pixmaps:/run/current-system/sw/share/icons:/run/current-system/sw/share/pixmaps";
 
       "io.github.equicord.equibop".Context = {
-      filesystems = [
-        "/home/catdealer/" # Expose user directory
-      ];
+        filesystems = [
+          "/home/catdealer/" # Expose user directory
+        ];
+      };
     };
   };
 }
