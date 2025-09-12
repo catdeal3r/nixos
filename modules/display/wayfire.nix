070d02cfe5f5b1c3945c63069a4d8541b35b0830
dealerofallthecats <xhackerqwertyx@protonmail.com>
2025-09-12 16:28:22 +1200

    Fri 12 Sep 2025 04:28:22 PM

--- modules/display/wayfire.nix
+++ modules/display/wayfire.nix
@@ -4,6 +4,12 @@
 
   programs.wayfire = {
     enable = true;
+
+    plugins = with pkgs.wayfirePlugins; [
+      wcm
+      wf-shell
+      wayfire-plugins-extra
+    ];
   };
 
   environment.systemPackages = with pkgs; [
