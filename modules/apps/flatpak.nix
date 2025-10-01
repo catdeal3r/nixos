7b56bba437314f2671b19a23cd7495b1f37878dd
dealerofallthecats <xhackerqwertyx@protonmail.com>
2025-10-01 18:02:07 +1300

    Wed 1 Oct 2025 06:02:07 PM

--- modules/apps/flatpak.nix
+++ modules/apps/flatpak.nix
@@ -24,6 +24,18 @@
     ];
     
     overrides = {
+      global = {
+        Context.sockets = [ "wayland" "org.freedesktop.Notifications" "!x11" "!fallback" ];      
+
+        Environment = {
+          # Fix un-themed cursor in some Wayland apps
+          XCURSOR_PATH = "/run/host/user-share/icons:/run/host/share/icons";
+
+          # Force correct theme for some GTK apps
+          GTK_THEME = "Adwaita:dark";
+        };
+      };
+
       "io.github.equicord.equibop".Context = {
         filesystems = [
           "/home/catdealer/" # Expose user directory and data drive
